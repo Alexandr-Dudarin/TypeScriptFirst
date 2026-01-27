ab8e352827575d30216a51ee9e91f12990b17e7d
Alexandr <stitel23@mail.ru>
2026-01-27 19:32:55 +0300

    add Lets

--- index.js
+++ index.js
@@ -2,3 +2,28 @@ var element = document.getElementById("ForTS");
 if (element) {
     element.textContent = "Hello TypeScript!";
 }
+var text = "Hi, my name is Alexander";
+var age = 28;
+var woman = false;
+var n = null;
+var postId = undefined;
+var height = [180, 178, 188, 196, 185];
+var length = height.length;
+console.log(text);
+console.log(age);
+console.log(woman);
+console.log(null);
+console.log(postId);
+console.log(height);
+console.log(length);
+var age = 55;
+var woman = true;
+console.log(age);
+console.log(woman);
+height.push(201);
+height.push(183);
+height.push(177);
+height.pop();
+console.log(height.length);
+height.sort(function (a, b) { return a - b; });
+console.log(height);
--- index.ts
+++ index.ts
@@ -2,3 +2,44 @@ const element = document.getElementById("ForTS");
 if (element) {
   element.textContent = "Hello TypeScript!";
 }
+
+let text : string = "Hi, my name is Alexander";
+
+let age : number = 28;
+
+let woman : boolean = false;
+
+let n : null = null;
+
+let postId : undefined = undefined;
+
+let height : number[] = [180,178,188,196,185];
+
+let length = height.length;
+
+console.log(text);
+console.log(age);
+console.log(woman);
+console.log(null);
+console.log(postId);
+console.log(height);
+console.log(length);
+
+let age : number = 55;
+
+let woman : boolean = true;
+
+console.log(age);
+console.log(woman);
+
+height.push(201);
+height.push(183);
+height.push(177);
+
+height.pop();
+
+console.log(height.length);
+
+height.sort((a, b) => a - b);
+
+console.log(height);
